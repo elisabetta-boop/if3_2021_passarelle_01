385b0b35e7b1cf8a0147d5dea5d20fd7f49df988
Elisabetta Saporetti <elisabetta.saporetti@yahoo.it>
2021-06-25 10:07:52 +0200

    initial commit

---
+++ Assets/Scripts/FirstComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstComponent : MonoBehaviour
+{
+    public float rotationSpeed;
+    public bool reverse;
+
+    public Vector3 rotationAxis;
+
+    void Start() {
+        Debug.Log("inside the start method");
+    }
+    void Update() {
+        float rotation = rotationSpeed * Time.deltaTime;
+        //Debug.Log("into the update");
+        if (reverse) {
+            rotation *=-1;
+        }
+
+        // rotationAxis = new Vector3(0,1,0);
+
+        transform.Rotate(rotation * rotationAxis, Space.World); 
+    } 
+}
