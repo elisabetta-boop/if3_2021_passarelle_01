c5e0916e58672e2ccd822fe954ecea1e7c2b388e
Elisabetta Saporetti <elisabetta.saporetti@yahoo.it>
2021-06-25 15:42:44 +0200

    script movements

--- Assets/Scripts/ObjectMovement.cs
+++ Assets/Scripts/ObjectMovement.cs
@@ -4,41 +4,55 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    public float speed;
-    public Vector3 direction;
-
+    // public float speed;
+    // public Vector3 direction;
     //public bool isRotation;
-    
-
     public enum MovementType {
         Rotation,
         Translation
     }
     //public ObjectMovement.MovementType type;
-    public MovementType movementType;
+    //public MovementType movementType;
+    [System.Serializable]
+    public struct Movement {
+        public MovementType type;
+        public float speed;
+        public Vector3 direction;
+        public Space space;
+    }
+   
+    public List<Movement> movements;
+    //public Movement mov;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (movementType == MovementType.Rotation) {
-            transform.Rotate(direction*speed*Time.deltaTime, Space.World);
-        } else if (movementType == MovementType.Translation) {
-            transform.Translate(direction*speed*Time.deltaTime, Space.World);
-        }
+    {   
+        foreach(Movement mov in movements) {
 
-        // // switch(movementType) {
+            
+
+            if (mov.type == MovementType.Rotation) {
+                transform.Rotate(mov.direction.normalized *mov.speed*Time.deltaTime, mov.space );
+            } else if (mov.type == MovementType.Translation) {
+                transform.Translate(mov.direction.normalized*mov.speed*Time.deltaTime, mov.space);
+            }
+    
+
+        // // switch(mov.Type) {
         // //     cases MovementType.Rotation:
-        // //         Rotate
+        // //         tranform.Rotate(mov.direction.normalized* mov.speed * Time.deltaTime,Space.World)
         // //         break;
         // //     Case MovementTiype.Translation:
-        // //         Translate
+        // //         Translate(mov.direction.normalized* mov.speed * Time.deltaTime,Space.World)
         // //         break;
         // // }
-        
+    }   
     }
 }
